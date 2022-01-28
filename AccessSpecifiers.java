b1d05636108bc17adb318135b8846076ce5f76a9
Pasham-Shivani <97822143+Pasham-Shivani@users.noreply.github.com>
2022-01-28 17:03:31 +0530

    Delete AccessSpecifiers.java

--- AccessSpecifiers.java
+++
@@ -1,9 +0,0 @@
-package phase1_assignments;
-import phase1_Assistedpractice.*;
-public class AccessSpecifiers {
-	public static void main(String[] args) {
-		PublicSpecifier obj = new 	PublicSpecifier(); 
-        obj.display();  
-
-	}
-}
