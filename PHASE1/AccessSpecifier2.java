6b8f7b64e69dc5b0b48c0a8b03c12d63ab86bcf3
Pasham-Shivani <97822143+Pasham-Shivani@users.noreply.github.com>
2022-01-28 16:57:55 +0530

    Delete PHASE1 directory

--- PHASE1/AccessSpecifier2.java
+++
@@ -1,9 +0,0 @@
-package phase1_assignments;
-import phase1_Assistedpractice.*;
-public class AccessSpecifier2 extends ProtectSpecifier{
-		public static void main(String[] args) {
-			AccessSpecifier2 obj = new AccessSpecifier2 ();   
-		       obj.display();  
-		}
-
-}
