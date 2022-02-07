0e7420204de4365e7e9643224616b143968ba3d8
Pasham-Shivani <97822143+Pasham-Shivani@users.noreply.github.com>
2022-02-07 16:51:19 +0530

    Delete ProtectSpecifier.java

--- ProtectSpecifier.java
+++
@@ -1,8 +0,0 @@
-package phase1_Assistedpractice;
-
-public class ProtectSpecifier {
-	protected void display() 
-    { 
-        System.out.println("This is protected access specifier"); 
-    } 
-}
